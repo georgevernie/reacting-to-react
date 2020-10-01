7ca1551b016becf0f31e6aa1f90df606b7a0a493
georgevernie <jonlangner@gmail.com>
2020-10-01 15:47:25 -0500

    Improved some understanding on the overall structure of React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,63 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
+
 /*
 function App(props) {
     return <h1>Hello {props.name}! Welcome to {props.city}.</h1>;
 }
 */
+
 //Typescript
 interface AppProps {
-    name?: "James";
-    city?: "Elks Town";
-  }
+  name?: "James";
+  city?: "Elks Town";
+  hasLoaded?: Boolean;
+}
 
 class App extends Component<AppProps>{
+    constructor(Component){
+      super(Component);
+      this.state = {
+        loaded: false,
+        hasLoaded:false,
+      };
+      this.handleClick = this.handleClick.bind(this);
+    }
+    componentDidMount(){
+      console.log("Inside Component Did Mount!");
+      this.setState({
+        ready: true
+      });
+    }
+    handleClick(e) {
+      this.setState({
+          loaded: true,
+      });
+    }
+    
     render() {
-        return (
-          <div>
-            Hello {this.props.name}! Welcome to {this.props.city}.
-          </div>
+      if(this.state.loaded){
+        console.log("success");
+        return(
+        <Fragment>{this.props.name}: {this.props.onChange}</Fragment>
         );
       }
+      else if(this.props.hasLoaded === false){
+        return(
+          <Fragment>
+            <h2>Loading...</h2>
+            <button onClick={this.handleClick}>Greet Anyway</button>
+          </Fragment>
+        );
+      }  
+      else{
+        return (  
+          <Fragment>
+            <h1>Hello {this.props.name}! Welcome to {this.props.city}.</h1>
+            <button onClick={this.handleClick}>Greet</button>
+          </Fragment>
+        );
+      }
+    }
 }
 
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+let hasLoaded = true;
 
-
-
-ReactDOM.render(<App name="James" city="Elks Town"/>, document.getElementById("root"));
+ReactDOM.render(<App name="James" city="Elks Town" onChange="Hello Joe." hasLoaded={hasLoaded}/>, document.getElementById("root"));
 
 
